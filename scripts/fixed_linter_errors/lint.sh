9edf22e743f8b5adf5e29792400f7900b0b05635
Aniket <anidok2008@gmail.com>
2019-05-15 01:04:21 +0530

    Setup linters

---
+++ scripts/fixed_linter_errors/lint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+EXIT_CODE=0
+
+pylint src/fixed_linter_errors || EXIT_CODE=1
+
+mypy src/fixed_linter_errors || EXIT_CODE=1
+
+pycodestyle . || EXIT_CODE=1
+
+python -m flake8 src/fixed_linter_errors || EXIT_CODE=1
+
+exit ${EXIT_CODE}
